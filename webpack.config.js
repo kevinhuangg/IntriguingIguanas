7726fbcad023c6b81fad11e2bdceecd6661e74ad
Enoch Kim <enochkim91@gmail.com>
2017-04-06 12:03:53 -0700

    Adds webpack config file for building/bundling

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: "./client/index.js",
+  output: {
+    path: path.resolve(__dirname, 'public/build'),
+    filename: 'index.js'
+  },
+  watch: true,
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ["es2015", "react"]
+        }
+      }
+    ]
+  }
+};
