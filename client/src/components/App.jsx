53375b670416a406480d9da4222e6154a56774c2
Enoch Kim <enochkim91@gmail.com>
2017-04-07 12:24:07 -0700

    Adds update App.jsx file

---
+++ client/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+  render () {
+	  return (<div>Hello World!</div>)
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+  }
+}
+
+
+export default connect(mapStateToProps)(App);
