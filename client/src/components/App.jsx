730f8eb338b21b0c8828a193a8168ce58e0537aa
kevinhuangg <email.kevinh@gmail.com>
2017-04-07 13:45:08 -0700

    addlist component button

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,19 +2,27 @@ import React from 'react';
 import { connect } from 'react-redux';
 import List from './List.jsx';
 import AddTaskForm from './addTaskForm.jsx';
+import AddList from './AddList.jsx'
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+	  super(props);
+  
+    this.handleAddList = this.handleAddList.bind(this)
+  }
+
+  handleAddList() {
+
+  }
 
   render () {
   	// console.log('addTaskForm', AddTaskForm)
 	 return (
 	   <div>
-  	     <List/>
+	     <AddList/>
+  	   <List/>
 	     <AddTaskForm/>
-  	   </div> 
+  	 </div> 
 	 )
   }
 }
