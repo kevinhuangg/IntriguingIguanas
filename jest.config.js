3eec585203f1a1f10b69c94774ba7f045c32bb4d
Enoch Kim <enochkim91@gmail.com>
2017-04-10 11:50:14 -0700

    Modifies git ignore to ignore logs, updated jest.config and package.json to use enzyme within App.test.js

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+{
+    "moduleFileExtensions": [
+        "js",
+        "jsx"
+    ],
+    "moduleNameMapper": {
+        "Clock": "<rootDir>/app/components/Clock.jsx",
+        "CountdownForm": "<rootDir>/app/components/CountdownForm.jsx",
+        "Countdown": "<rootDir>/app/components/Countdown.jsx",
+        "App": "<rootDir>/client/src/components/App.jsx"
+    }
+}
--- tests/client/App.test.js
+++ tests/client/App.test.js
@@ -1,5 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from 'App'
+import { shallow, mount } from 'enzyme'
+
+
 describe('App', () => {
     it('should be able to run tests', () => {
         expect(1 + 2).toEqual(3);
+    });
+
+    it('should render Hello World!', () => {
+    	const app = mount(<App />);
+    	const text = <div>Hello World</div>;
+
+ 			expect(clock.contains(text)).toEqual(true);
     });
 });
