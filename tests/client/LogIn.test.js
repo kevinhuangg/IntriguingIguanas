c06f5d5ef91f793b9dd238eed59bb8fae39e4631
kevinhuangg <email.kevinh@gmail.com>
2017-04-10 14:49:00 -0700

    adding testing pages for login and signup

---
+++ tests/client/LogIn.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { App } from 'App'
+import { shallow, mount } from 'enzyme'
+import HomePage from 'HomePage'
+
+
+describe('LogInPage', () => {
+  it('should be able to run tests', () => {
+    expect(1 + 2).toEqual(3);
+  });
+});
