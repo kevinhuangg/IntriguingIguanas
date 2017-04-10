442ed3a5eb71280140c4366bf19f526b5c435172
kevinhuangg <email.kevinh@gmail.com>
2017-04-10 11:49:36 -0700

    adds button for login and signup

--- client/src/components/LogInPage.jsx
+++ client/src/components/LogInPage.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
 
-const LogInPage = () => {
-  return (
-  <div>
-  </div>
-  )
+class LogInPage extends React.Component {
+  constructor(props) {
+  	super(props)
+  	this.state = {
+  		username: '',
+  		password: ''
+  	}
+  
+  	this.handleUsernameChange = this.handleUsernameChange.bind(this)
+  	this.handlePasswordChange = this.handlePasswordChange.bind(this)
+  }
+
+  handlePasswordChange() {
+  	this.setState({ password: this.state.password })
+  }
+  handleUsernameChange() {
+  	this.setState({ username: this.state.username })
+  }
+
+  render() {
+	  return (
+	    <div>
+	    	<form> 
+	    	  <label>
+	    	    Username:
+            <input type='text' 
+              value={ this.state.username } 
+              onChange={ this.handleUsernameChange }
+            />
+          </label>
+          <label>
+            Password:
+            <input type='text'
+              value={ this.state.password }
+              onChange={ this.handlePasswordChange }
+            />
+          </label>
+          <input 
+            type='submit' 
+            value='Login'
+          />        
+	      </form>
+
+	    </div>
+	)
+  }
 }
 
 export default LogInPage
--- client/src/components/SignUpPage.jsx
+++ client/src/components/SignUpPage.jsx
@@ -1,10 +1,68 @@
 import React from 'react'
 
-const SignUpPage = () => {
-  return (
-  	<div>
-  	</div>
-  )
+class SignUpPage extends React.Component {
+  constructor(props) {
+  	super(props)
+  	this.state = {
+  		username: '',
+  		email: '',
+  		password: ''
+  	}
+  
+  	this.handleUsernameChange = this.handleUsernameChange.bind(this)
+  	this.handlePasswordChange = this.handlePasswordChange.bind(this)
+  	this.handleEmailChange = this.handleEmailChange.bind(this)
+  	this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handlePasswordChange() {
+  	this.setState({ password: this.state.password })
+  }
+  handleUsernameChange() {
+  	this.setState({ username: this.supertate.username })
+  }
+  handleEmailChange() {
+  	this.setState({ email: this.state.email })  	
+  }
+  handleSubmit() {
+
+  }
+
+  render() {
+	  return (
+	    <div>
+	      <form> 
+	    	<label>
+	    	  Username:
+              <input 
+                type='text' 
+                value={ this.state.username } 
+                onChange={ this.handleUsernameChange }
+              />
+            </label>
+            <label>
+              Email:
+              <input 
+                type='text'
+                value={ this.state.email }
+                onChange={ this.handleEmailChange }
+              />
+            </label>
+            <label>
+              Password:
+              <input type='text'
+                value={ this.state.password }
+               onChange={ this.handlePasswordChange }
+              />
+            </label>
+            <input 
+              type='submit' 
+              value='Signup'
+            />
+	      </form>
+	    </div>
+	)
+  }
 }
 
 export default SignUpPage
