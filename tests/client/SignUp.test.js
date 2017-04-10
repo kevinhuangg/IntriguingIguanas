079aa5f2d6a558012c0425dc1df956c5ac7e3389
kevinhuangg <email.kevinh@gmail.com>
2017-04-10 15:10:15 -0700

    import pages to test pages

--- tests/client/SignUp.test.js
+++ tests/client/SignUp.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from 'App'
 import { shallow, mount } from 'enzyme'
-import HomePage from 'HomePage'
+import SignUpPage from 'SignUpPage'
 
 
 describe('SignUpPage', () => {
