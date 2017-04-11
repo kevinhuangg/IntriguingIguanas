1167cad731ffab3943eda3c5392d6cf7636abee8
Allen An Le <allenanle16@gmail.com>
2017-04-11 14:15:13 -0700

    Add actions/reducers to fetch lists & GET req to server

--- client/src/actions/List.js
+++ client/src/actions/List.js
@@ -1,25 +1,53 @@
 import axios from 'axios'
 
-// ---------- CREATE ----------
-function listCreated() {
+// ------------ CREATE ------------
+export const listCreated = () => {
   return {
     type: 'LIST_CREATED'
   }
 }
 
-function listError(error) {
+export const createError = (error) => {
   return {
-    type: 'ERROR',
+    type: 'CREATE_ERROR',
     error
   }
 }
 
-export createList = () => {
+export const createList = (listName, board_id) => {
   return (dispatch) => {
-    axios.post('/list/create', { listName })
-    .then(data => dispatch(listCreated()))
-    .catch(error => dispath(listError(error)))
+    axios.post('/list/create', { listName, board_id })
+    .then(res => dispatch(listCreated()))
+    .catch(error => dispatch(createError(error)))
   }
 }
 
-export default List
+// ------------ FETCH ------------
+export const fetchingLists = () => {
+  return {
+    type: 'FETCHING_BOARDS'
+  }
+}
+
+export const listsFetched = (lists) => {
+  return {
+    type: 'LISTS_FETCHED',
+    allLists: lists
+  }
+}
+
+export const fetchError = (error) => {
+  return {
+    type: 'FETCH_ERROR',
+    error: error
+  }
+
+export const fetchLists = () => {
+  return (dispatch) => {
+    dispatch(fetchingLists())
+
+    axios.post('/list/get', { board_id })
+    .then(res => dispatch(listFetched(res.data)))
+    .catch(error => dispatch(fetchError(error)))
+  }
+}
--- client/src/reducers/List.js
+++ client/src/reducers/List.js
@@ -1,21 +1,43 @@
 const initialState = {
-  error: null
+  allList: [],
+  createError: null,
+  fetching: false,
+  fetchError: null
 }
 
-function createBoard(state = initialState, action) {
+
+export const List = (state = initialState, action) => {
   switch (action.type) {
+    // ------------ CREATE ------------
     case 'LIST_CREATED':
       return {
         ...initialState
       }
-    case 'ERROR':
+    case 'CREATE_ERROR':
       return {
         ...state,
         error: action.error
       }
+
+    // ------------ FETCH ------------
+    case 'FETCHING_LISTS':
+      // for UX purpose – might take long to fetch
+      return {
+        ...state,
+        fetchingLists: true
+      }
+    case 'LISTS_FETCHED':
+      return {
+        ...state,
+        boardList: action.lists
+      }
+    case 'FETCH_ERROR':
+      return {
+        ...state,
+        error: action.error
+      }
+
     default:
       return state
   }
 }
-
-export default List
