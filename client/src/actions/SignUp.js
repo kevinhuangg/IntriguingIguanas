c52f3c73495a59b6b460085fe3f33d040e4b5d83
Christine Dey <christinedey5@gmail.com>
2017-04-11 14:11:52 -0700

    Modify SignUp action

--- client/src/actions/SignUp.js
+++ client/src/actions/SignUp.js
@@ -7,21 +7,32 @@ export function SignUpRequest(username, email, password) {
   }
 }
 
-export function SigningIn() {
+export function SigningUp() {
   return {
-    type: 'SIGNING_IN'
+    type: 'SIGNING_UP'
   }
 }
 
-export function SignUpFailure() {
+export function SignUpFailure(error) {
   return {
     type: 'SIGN_UP_FAILURE',
+    error
   }
 }
 
-export function SignUpSuccess() {
+export function SignUpSuccess(data) {
   return {
-    type: 'SIGN_UP_SUCCESS'
+    type: 'SIGN_UP_SUCCESS',
+    data
+  }
+}
+
+export function SignUp(username, email, password) {
+  return (dispatch) => {
+    dispatch(SigningUp())
+    axios.post('', {username, email, password})
+    .then(data => dispatch(SignUpSuccess(data)))
+    .catch(error => dispatch(SignUpFailure(error)))
   }
 }
 
