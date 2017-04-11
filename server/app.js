4822092f01c785331e17f41ee95dddcebf0fc48c
kevinhuangg <email.kevinh@gmail.com>
2017-04-10 18:32:53 -0700

    add cookieparser and express session as dependencies

--- server/app.js
+++ server/app.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const path = require('path');
+const passport = require('passport');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const auth = require('./routes/auth.js');
+
 
 const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(cookieParser());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
 // Sets up /client as static directory
 app.use(express.static(__dirname + '/../client/dist'));
+
+//Authentication	
+
 
 // deployment port variable - default to 3000
 var port = process.env.PORT || 3000
