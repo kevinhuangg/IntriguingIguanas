f23fe77171ea3662c11f296708c137a7047a7dac
kevinhuangg <email.kevinh@gmail.com>
2017-04-11 16:10:59 -0700

    refactor code
    remove comments

--- client/src/reducers/LogIn.js
+++ client/src/reducers/LogIn.js
@@ -6,12 +6,6 @@ let initialState = {
 
 const LogIn = (state=initialState, action) => {
   switch (action.type) {
-    // case 'LOG_IN_REQUEST':
-    //   return {
-    //     ...state,
-    //     username: action.username,
-    //     password: action.password
-    //   }
     case 'LOGGING_IN':
       return {
         ...state,
