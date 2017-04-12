8b044de9cc45b40641a73f58398e88190855a1f1
Enoch Kim <enochkim91@gmail.com>
2017-04-12 16:01:42 -0700

    Adds basic socket functionality, built out connection and join board listeners

--- server/socket.js
+++ server/socket.js
@@ -1,5 +1,43 @@
+var sockets = require('sockets.io');
+var io;
+//import database queries
+
+var rooms = [];
+var currentBoard = null;
+
 
 
 module.exports = {
-  //export sockets to app.js
+	io: function() {
+		return io;
+	},
+	init: function(server) {
+		io = sockets(server);
+
+		io.on('connection', function(socket) {
+			console.log('Connected to ' + socket);
+			
+			socket.on('join-board', function(board_id) {
+				socket.join(board, function () {
+					currentBoard = board;
+					console.log('joined ' + board)
+				});
+			})
+
+			socket.on('fetch-board', function (board_id) {
+				//query to the database to fetch specific board
+				// .then(result) => {
+					// socket.to(currentBoard).emit('board-fetched', result)
+				//}
+			});
+
+			socket.on('disconnect', function () {
+				console.log('client disconnected')
+			});
+		})
+
+		return io;
+	}
+
+
 }
