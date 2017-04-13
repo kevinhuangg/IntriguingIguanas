a1969e81348e58e83a75ade15b48218365d2f2aa
kevinhuangg <email.kevinh@gmail.com>
2017-04-13 14:20:34 -0700

    db queries for addList, editListName, and deleteList

---
+++ database/db-queries/list.js
@@ -0,0 +1,18 @@
+const db = require('../db.js');
+
+module.exports = {
+  //add list to db
+  addList: (name, boardID) =>{
+    return db.query(`INSERT INTO lists (listname, board_id) VALUES ('${name}', ${boardID})`) 
+  }
+  //edit list name in db
+  editListName: (name, boardID) => {
+    return db.query(`UPDATE lists SET listname='${name}' WHERE board_id=${boardID}`)
+  }
+
+  //delete task from db using list id
+  deleteList: (listID) => {
+    return db.query(`DELETE FROM lists WHERE id=${listID}`)
+  }
+
+}
