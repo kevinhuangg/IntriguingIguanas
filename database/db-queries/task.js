66636765fdd3681fbcce6fa194fc658f5744eff7
Christine Dey <christinedey5@gmail.com>
2017-04-13 15:25:20 -0700

    Syntax edits

--- database/db-queries/task.js
+++ database/db-queries/task.js
@@ -5,12 +5,12 @@ module.exports = {
     return db.query(`DELETE FROM tasks WHERE id=${task_id}`);
   },
   editTask: (task_id, text) => {
-    return db.query(`UPDATE tasks SET text='${text}' where id=${task_id}`);
+    return db.query(`UPDATE tasks SET text='${text}' WHERE id=${task_id}`);
   },
   addTask: (text) => {
     return db.query(`INSERT INTO tasks (text) VALUES ('${text}')`);
   },
   assignTask: (task_id, user) => {
-    return db.query(`UPDATE tasks SET assigned='${user}' where id=${task_id}`);
+    return db.query(`UPDATE tasks SET assigned='${user}' WHERE id=${task_id}`);
   }
 }
