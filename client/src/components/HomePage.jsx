0041f07600f73704950ef2ddbbb68058062ec657
kevinhuangg <email.kevinh@gmail.com>
2017-04-15 10:16:08 -0700

    tinkering with design

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -10,13 +10,31 @@ export class HomePage extends React.Component {
   render() {
     return (
       <div>
+      <div className="ui large top fixed menu transition visible">
+        <div className="ui container">
+          <div className="right menu">
+            <div className="item" onClick={ this.props.route.sendToLogin }>
+              <a className="ui button">Log in</a>
+            </div>
+            <div className="item" onClick={ this.props.route.sendToSignup }>
+              <a className="ui primary button">Sign Up</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h1>Root.io is your one stop shop with all the tools necessary for live and remote collaboration.</h1>
         <h1>Root.io is your one stop shop with all the tools necessary for live and remote collaboration.</h1>
         <h2>With shared project boards and video chat, your team can manage tasks seamlessly.</h2>
         <button onClick={ this.props.route.sendToSignup }>Sign Up</button>
         <div onClick={ this.props.route.sendToLogin }>Already a Root.io user? Log in.</div>
       </div>
+      </div>
+
+
     )
   }
 }
 
 export default HomePage
+
