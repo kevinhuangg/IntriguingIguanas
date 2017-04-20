83ff93eeaf7a91d7972490d4183addb9f342859d
kevinhuangg <email.kevinh@gmail.com>
2017-04-19 18:28:09 -0700

    small changes

--- database/tableConfig.js
+++ database/tableConfig.js
@@ -9,6 +9,43 @@ module.exports = (db) => {
     );\
   ')
   .then(() => {
-    
+    return db.query('\
+      CREATE TABLE IF NOT EXISTS boards (\
+        id INT PRIMARY KEY,\
+        boardname VARCHAR(30) NOT NULL\
+      );\
+    ')
+  })
+  .then(() => {
+    return db.query('\
+      CREATE TABLE IF NOT EXISTS users_boards (\
+        id INT PRIMARY KEY,\
+        user_id INT FOREIGN KEY,\
+        board_id INT FOREIGN KEY,\
+        access integer[],\
+        starred boolean\
+      );\      
+    ')
+  })
+  .then(() => {
+    return db.query('\
+      CREATE TABLE IF NOT EXISTS lists (\
+        id INT PRIMARY KEY,\
+        board_id INT FOREIGN KEY,\
+        listname VARCHAR(30) NOT NULL,\
+        current_order INT\
+      );\
+    ')
+  })
+  .then(() => {
+    return db.query('\
+      CREATE TABLE tasks (\
+        id INT PRIMARY KEY,\
+        list_id INT FOREIGN KEY,\
+        "text" VARCHAR(250),\
+        assigned integer[],\
+        current_order NOT NULL\
+      );\
+    ')
   })
 }
